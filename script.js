145e3633573fe6da7fe905a4ddd42e299abd93e5
cheilaribeiro19 <cheilaribeiro19@gmail.com>
2026-08-10 14:07:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ button.addEventListener("click", function() {
     if (input.value !== "") {
         const newTask = document.createElement("li");
         newTask.textContent = input.value;
+
+        newTask.addEventListener("click", function() {
+            newTask.style.textDecoration = "line-through";
+        });
+
         list.appendChild(newTask);
         input.value = "";
     }
